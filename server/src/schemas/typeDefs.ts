1946bff727ca4251c569807782fc0a4afb856aa7
Connor Dailey <connorldailey@gmail.com>
2024-12-31 12:28:18 -0500

    Add typeDefs for graphql api

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,56 @@
+const typeDefs = `
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String!
+        title: String!
+        authors: [String]
+        description: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: String!
+        user: User!
+    }
+
+    input BookInput {
+        bookId: String!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(
+            email: String!
+            password: String!
+        ): Auth
+
+        addUser(
+            username: String!, 
+            email: String!, 
+            password: String!
+        ): Auth
+
+        saveBook(bookData: BookInput!): User
+
+        removeBook(bookId: String!): User
+        
+    }
+`;
+
+export default typeDefs;
